bb3a6e0757e00174737f26e3bc39e29544e3d5ae
sana1298 <sanoferbavasa@gmail.com>
2023-07-19 13:30:12 +0530

    final

---
+++ app.js
@@ -0,0 +1,25 @@
+let title=document.getElementById("title");
+let notes=document.getElementById("notes");
+let btn=document.getElementById("btn");
+let add=document.getElementById("add");
+let form=document.getElementById("form");
+add.addEventListener("click",(e) =>{
+    e.preventDefault();
+      let form=document.getElementById("form");
+     form.style.display="block";
+
+})
+let array=[];
+btn.addEventListener("click",(e) =>{
+    e.preventDefault();
+    form.style.display="none";
+    // let ul_list=document.getElementById("ul-list");
+    // let list=document.createElement("li");
+    let value= title.value
+    array.push(value);
+    console.log(array)
+   console.log(value);
+    // ul_list.appendChild(list);
+    // ul_list.appendChild(array[0]);
+    add.appendChild(array[0])
+});
